4c3289f2a9c9a4e7e50e1c536b9b83c54b9844a7
YukiMiyatake <YukiMariaMiyatake@gmail.com>
2022-04-05 02:50:18 +0900

    It work

--- EldenTalk/onconnect/app.js
+++ EldenTalk/onconnect/app.js
@@ -1,16 +1,16 @@
 var AWS = require("aws-sdk");
 AWS.config.update({ region: process.env.AWS_REGION });
 //AWS.config.update({ region: 'localhost' });
-//var DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
-
+var DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+/*
 var DDB = process.env.IS_OFFLINE
 ? new DynamoDB({
-    region: 'localhost',
-    endpoint: 'http://localhost:8000',
+    region: 'ap-northeast1',
+    endpoint: 'http://172.20.191.9:8000',
     apiVersion: "2012-10-08",
   })
   : new DynamoDB({ apiVersion: "2012-10-08" });
-
+*/
 exports.handler = function (event, context, callback) {
   var putParams = {
     TableName: process.env.TABLE_NAME,
--- EldenTalk/sendmessage/app.js
+++ EldenTalk/sendmessage/app.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
 
+AWS.config.update({ region: process.env.AWS_REGION });
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 const { TABLE_NAME } = process.env;
