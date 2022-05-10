74672435446f36bdbfedc9936d64e5469e9a500e
YukiMiyatake <YukiMariaMiyatake@gmail.com>
2022-05-10 21:23:01 +0900

    basic fsm

--- Sandbox/GoFSM/go.mod
+++ Sandbox/GoFSM/go.mod
@@ -1,3 +1,5 @@
 module fsm-test
 
 go 1.18
+
+require github.com/looplab/fsm v0.3.0
--- Sandbox/GoFSM/main.go
+++ Sandbox/GoFSM/main.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/looplab/fsm"
+)
+
+const (
+	start  = "start"
+	middle = "middle"
+	end    = "end"
+	finish = ""
 )
 
 func main() {
+
+	eventTree := fsm.Events{
+		{Name: start, Src: []string{start}, Dst: middle},
+		{
+			Name: middle,
+			Src:  []string{start},
+			Dst:  end,
+		},
+		{
+			Name: end,
+			Src:  []string{end, start},
+			Dst:  finish,
+		},
+		{
+			Name: finish,
+			Src:  []string{finish, middle},
+			Dst:  finish,
+		},
+	}
+
+	ownFsm := fsm.NewFSM(
+		start,
+		eventTree,
+		fsm.Callbacks{},
+	)
+
+	ownFsm.Current()    // start
+	ownFsm.Event(start) // start eventが発火し、Dstのnodeへ遷移
+	ownFsm.Current()    // middle
+
+	//fsm.VisualizeWithType(ownFsm,fsm.GRAPHVIZ)
+	// http://www.webgraphviz.com/
 	fmt.Println("Hello")
 }
